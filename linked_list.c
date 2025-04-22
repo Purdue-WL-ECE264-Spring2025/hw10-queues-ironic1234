5f133d41de65c818d7c8355fb21a8b181a1eb0c7
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-04-22 15:37:26 -0400

    solved

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,67 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+    struct list_node * node = (struct list_node *) malloc(sizeof(struct list_node));
+    node->value = value;
+    node->next = NULL;
+    return node;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+    struct list_node * node = new_node(value);
+    node->next = list->head;
+    list->head = node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+    struct list_node * node = new_node(value);
+    struct list_node * current_node = list->head;
+    if (current_node != NULL) {
+        while (current_node->next != NULL) {
+            current_node = current_node->next;
+        }
+        current_node->next = node;
+    } else {
+        list->head = node;
+    }
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+    struct list_node * temp = list->head;
+    size_t value = temp->value;
+    list->head = temp->next;
+    free(temp);
+    return value;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) {
+    struct list_node * current = list->head;
+    struct list_node * previous = NULL;
+    if (current->next == NULL) {
+        size_t value = current->value;
+        free(current);
+        list->head = NULL; 
+        return value;
+    }
+    while (current->next != NULL) {
+        previous = current;
+        current = current->next;
+    }
+    size_t value = current->value;
+    previous->next = NULL;
+    free(current);
+    return value;
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+    struct list_node * current = list.head;
+    while (current != NULL) {
+        struct list_node * next = current->next;
+        free(current);
+        current = next;
+    }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,97 @@
 #include "queue.h"
+#include "linked_list.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    insert_at_head(&q->data, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+    size_t value = remove_from_tail(&q->data);
+    return deserialize(value);
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+bool are_states_same(struct game_state state1, struct game_state state2) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (state1.tiles[i][j] != state2.tiles[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool is_visited(struct linked_list *visited, struct game_state state) {
+    struct list_node *current = visited->head;
+    struct game_state visited_state;
+    while (current != NULL) {
+        visited_state = deserialize(current->value);
+        if (are_states_same(visited_state, state)) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
+}
+
+struct game_state zero_num_steps(struct game_state state) {
+    state.num_steps = 0;
+    return state;
+}
+
+int number_of_moves(struct game_state start) { 
+    struct queue q = {0};
+    static bool visited[10007];
+
+    enqueue(&q, start);
+
+    visited[serialize(zero_num_steps(start)) % 10007] = true;
+
+    struct game_state solved = {
+        .tiles = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,0}
+        },
+    };
+
+    while (q.data.head != NULL) {
+        struct game_state current = dequeue(&q);
+        if (are_states_same(current, solved)) {
+            free_list(q.data);
+            return current.num_steps;
+        }
+        struct game_state test_state = current;
+
+        move_up(&test_state);
+        if (!are_states_same(test_state, current) && !visited[serialize(zero_num_steps(test_state)) % 10007]) {
+            enqueue(&q, test_state);
+            visited[serialize(zero_num_steps(test_state)) % 10007] = true;
+        }
+
+        test_state = current;
+        move_down(&test_state);
+        if (!are_states_same(test_state, current) && !visited[serialize(zero_num_steps(test_state)) % 10007]) {
+            enqueue(&q, test_state);
+            visited[serialize(zero_num_steps(test_state)) % 10007] = true;
+        }
+
+        test_state = current;
+        move_left(&test_state);
+        if (!are_states_same(test_state, current) && !visited[serialize(zero_num_steps(test_state)) % 10007]) {
+            enqueue(&q, test_state);
+            visited[serialize(zero_num_steps(test_state)) % 10007] = true;
+        }
+
+        test_state = current;
+        move_right(&test_state);
+        if (!are_states_same(test_state, current) && !visited[serialize(zero_num_steps(test_state)) % 10007]) {
+            enqueue(&q, test_state);
+            visited[serialize(zero_num_steps(test_state)) % 10007] = true;
+        }
+    }
+    free_list(q.data);
+    return 0; 
+}
